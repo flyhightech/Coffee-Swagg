0e2f277ea74002d53129694a59db7194434e9f61
Bernard Huff <bernard.huff008@gmail.com>
2018-12-29 00:14:00 -0600

    Initial Commit

---
+++ Coffee-Swagg/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Coffee-Swagg
+//
+//  Created by Bernard Huff on 12/29/18.
+//  Copyright © 2018 Flyhightech.LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
